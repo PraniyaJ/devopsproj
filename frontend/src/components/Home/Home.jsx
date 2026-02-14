a6b3509aa85db47ffaf52dfc64ccba1822b4711b
T.H.D.P.U Jayasekara <praneethjayasekara2002@gmail.com>
2026-02-14 15:21:49 +0530

    Update header text to include 'Praneeth'

--- frontend/src/components/Home/Home.jsx
+++ frontend/src/components/Home/Home.jsx
@@ -17,7 +17,7 @@ const Home = () => {
             </p>
 
             <h1 className="text-4xl md:text-5xl font-extrabold leading-tight">
-              Trusted, Compassionate Healthcare
+              Praneeth, Compassionate Healthcare
               {/* Efficient, Secure and Always Dedicated to Your Care */}
             </h1>
 
